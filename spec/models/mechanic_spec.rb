f67461bd879eabe2eb06048a37e6913092e12c63
GregJMitchell <g2mitchell3003@gmail.com>
2020-10-23 11:11:36 -0600

    Create test for order rides method

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,8 +1,20 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Mechanic, type: :model do
-  describe "relationships" do
-    it {should have_many :ride_mechanics}
-    it {should have_many(:rides).through(:ride_mechanics)}
+  describe 'relationships' do
+    it { should have_many :ride_mechanics }
+    it { should have_many(:rides).through(:ride_mechanics) }
+  end
+  it 'Should order rides in alphabetical order' do
+    mechanic = Mechanic.create(name: 'Sal', experience: 5)
+    amusement_park = AmusementPark.create(name: 'Turing Land', admission: 27.95)
+    ride_1 = Ride.create(name: 'Ruby Coaster', thrill: 10, amusement_park_id: amusement_park.id)
+    ride_2 = Ride.create(name: 'Java Coaster', thrill: 1, amusement_park_id: amusement_park.id)
+
+    RideMechanic.create(ride_id: ride_1.id, mechanic_id: mechanic.id)
+    RideMechanic.create(ride_id: ride_2.id, mechanic_id: mechanic.id)
+    expect(mechanic.order_rides).to eq([ride_2, ride_1])
   end
 end
